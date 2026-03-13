e50fc494ab704c26fe5f6e0b0454a9008ef71bcf
ashbbdu <ashishsrivastava7007@gmail.com>
2026-03-13 14:46:30 +0530

    initial commit

---
+++ src/EscapeSequence.java
@@ -0,0 +1,8 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("hello 'KGCoding'...");
+        System.out.println("hello \"KGCoding\"...");
+        System.out.println("hello \\KGCoding\\...");
+        System.out.println("hello \nKGCoding...");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a ;
+        a = 10;
+        System.out.println(a);
+
+        float myFloat = 10f;
+        System.out.println(myFloat);
+
+
+    }
+}
