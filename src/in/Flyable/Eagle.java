8541fab89d2a0efcdde86a8d95e095ff3588a6e7
ashbbdu <ashishsrivastava7007@gmail.com>
2026-03-22 12:44:01 +0530

    done

--- src/in/Flyable/Eagle.java
+++ src/in/Flyable/Eagle.java
@@ -3,6 +3,7 @@ package in.Flyable;
     public class Eagle extends Bird {
 
     // this is required
+    //        "Call the constructor of the parent class (Bird) and pass 'Eagle' as breed"
     public Eagle () {
         super("Eagle");
     }
