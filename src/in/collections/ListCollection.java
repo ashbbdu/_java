17787328bdd12b9907eb1020edbe865c74013be4
ashbbdu <ashishsrivastava7007@gmail.com>
2026-04-03 00:39:20 +0530

    wip

--- src/in/collections/ListCollection.java
+++ src/in/collections/ListCollection.java
@@ -2,6 +2,7 @@ package in.collections;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ListCollection {
@@ -18,5 +19,18 @@ public class ListCollection {
 
         System.out.println(s);
 
+//        List
+
+        List <Integer> nc = new ArrayList<>();
+        nc.add(1);
+        System.out.println(nc);
+        nc.add(0 , 10);
+        System.out.println(nc);
+        System.out.println(nc.size());
+
+
+        for(Integer i : nc) {
+            System.out.println(i);
+        }
     }
 }
---
+++ src/in/collections/QueueTut.java
@@ -0,0 +1,35 @@
+package in.collections;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueTut {
+    public static void main(String[] args) {
+//        FIFO
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.add(1);
+        pq.add(2);
+//        System.out.println(pq.peek());
+//        System.out.println(pq.poll());
+
+//        System.out.println(pq);
+
+
+        Queue <Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        System.out.println(ll.size());
+        System.out.println(ll.peek());;
+
+        while (!ll.isEmpty()) {
+            System.out.println("Removed");
+            ll.remove();
+        }
+
+
+
+    }
+}
